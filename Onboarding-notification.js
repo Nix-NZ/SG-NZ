2f2c75946ea924c61d99b5ec48b4b2fc76139b5b
Nix-NZ <44488769+Nix-NZ@users.noreply.github.com>
2023-04-05 14:57:18 +1200

    Add files via upload

---
+++ Onboarding-notification.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const banner = document.querySelector('.notification-banner-onboarding');
+    const statusElement = document.querySelector('.onboarding-status');
+
+    if (banner && statusElement) {
+        const onboardingCompleted = statusElement.textContent.trim() === 'Yes';
+
+        if (onboardingCompleted) {
+            banner.style.display = 'none';
+        }
+    } else {
+        console.error('Could not find either the Notification-banner-onboarding or onboarding-status element');
+    }
+});
